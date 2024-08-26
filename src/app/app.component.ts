91a45c219b64c05953e4d374672e5bdde21b4815
VidhiRana24 <ranavidhi086@gmail.com>
2024-08-26 22:34:18 +0530

    Creating pipe Demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,20 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'pipesDemo';
+  currentDate = new Date()
+  currentTemperature ={
+    berlin :4.2749812,
+    newYork : 18.1214,
+    paris:72.1209001,
+    chicago:65.0775238,
+  }
+
+  historicTemperatures =[ 
+    25,  37,19, -4,28,21,19,28,33,31,9,11,5,-12,-5
+  ];
+ 
+  onReset(index:number){
+    this.historicTemperatures[index] = 18;
+  }
+
 }
